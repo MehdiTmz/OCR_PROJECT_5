aba4b4539de3edcfff5bc2f97a23cbf0e742361b
Mehdi <mehditmz1@gmail.com>
2022-11-08 00:29:29 +0100

    film specific info for modal updated

--- animation.js
+++ animation.js
@@ -4,7 +4,6 @@ function charge_image(item,list){
     number_of_image = image_table.length
     for(i = 0; i < list.length; i++){
         image_table[i].src = list[i]["image_url"]
-        //image_table[i].setAttribute('id' , list[i]["id"])
     }
 }
 
@@ -14,33 +13,34 @@ function charge_image_2(item,list){
   image_table[6].src = list[1]["image_url"]
 }
 
-// function recuperate_data(film){
-//   let data = [];
-//   for (x = 0; x < 5; x++){
-//     data.append([film[x]['title'], film[x]['year'], film['imdb_score'], film[x]['votes'], film[x]['directors'], film[x]['actors'], film[x]['writers'], film[x]['genres']]);
-//   };
-//   return data;
-// }
 function recuperate_data(film){
-  return [film['title'], film['year'], film['imdb_score'], film['votes'], film['directors'], film['actors'], film['writers'], film['genres']];
+            return ["Titre : " + film['title'], "Année : " + film['year'], "Score Imdb : "  + film['imdb_score'], "Nombre de vote : " + film['votes'], "Directeur : " + film['directors'], "Atceur : " +film['actors'], "Auteur : " + film['writers'], "Genres : " + film['genres'],"Durée : " + film["duration"], "Pays : " +  film['countries'], "Date de sortie : " + film["date_published"], "Resumé : " + film["long_description"], "Rated : " + film["rated"]];
 }
+async function test(id) {
+  return await fetch(
+    'http://localhost:8000/api/v1/titles/'+id,{
+      method: 'GET',
+      }
+      ).then(function(res) {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then(function(value) {
+        
+        for (x in recuperate_data(value)){
+          console.log('Here', recuperate_data(value)[x])
+        }
+        var modal = document.getElementById("myModal");
+        display_modal_text(modal, value)
+      })
+      .catch(function(err) {
+        // Une erreur est survenue
+      });
 
-// async function getResponse(url,item) {
-//     const response = await fetch(
-//       url,
-//       {
-//         method: 'GET'
-//         });
-    
-//     const data = await response.json();
-//     charge_image(item, data.results);
-
-//     //console.log(recuperate_data(data.results))
-//     test_modal();
-//     return data;
-    
-//   }
-async function getResponse(url,item) {
+  
+}
+async function getResponse(url,item, main = false) {
     return await fetch(
       url,{
         method: 'GET',
@@ -52,6 +52,9 @@ async function getResponse(url,item) {
         })
         .then(function(value) {
           console.log(value);
+          if (main){
+            document.getElementsByClassName("Main-film-container")[0].style = "background-image: url('" + value.results[0]["image_url"]+"')";        
+          } 
           charge_image(item, value.results);
           test_modal(item,value.results)
         })
@@ -86,6 +89,7 @@ async function getResponse_2(url,item) {
 function display_modal_text(modal,data){
   modal_text = modal.getElementsByTagName("p")
   modal_text_table = recuperate_data(data)
+  modal.getElementsByClassName('modal-image')[0].src = data["image_url"]
   for(j = 0; j < modal_text.length; j++){
     modal_text[j].textContent = modal_text_table[j];
   }
@@ -94,6 +98,7 @@ function test_modal(btn_name, data){
   // Get the modal
   var modal = document.getElementById("myModal");
   // Get the the modal name
+  var modal_img = modal.getElementsByTagName("modal-image")
   var btn = document.getElementsByClassName(btn_name);
 
   // Get the <span> element that closes the modal
@@ -102,25 +107,32 @@ function test_modal(btn_name, data){
   // When the user clicks on the buttons, open the modal
     btn[0].onclick = function click(){
     modal.style.display = "block";
-    display_modal_text(modal, data[0]);
+      display_modal_text(modal, data[0]);
+      test([data[0]['id']])
+    }
 
-  }
     btn[1].onclick = function click(){
     modal.style.display = "block";
-    display_modal_text(modal, data[1]);
+      display_modal_text(modal, data[1]);
+      test([data[1]['id']])
     }
 
     btn[2].onclick = function click(){
       modal.style.display = "block";
       display_modal_text(modal, data[2]);
+      test([data[2]['id']])
     }
+
     btn[3].onclick = function click(){
       modal.style.display = "block";
       display_modal_text(modal, data[3]);
+      test([data[3]['id']])
     }
+
     btn[4].onclick = function click(){
       modal.style.display = "block";
       display_modal_text(modal, data[4]);
+      test([data[4]['id']])
     }
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
@@ -150,8 +162,9 @@ function modif_modal(btn_name, data){
 
 }
 var response;
+
 getResponse('http://localhost:8000//api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
-"item-1").then(response);
+"item-1",true).then(response);
 getResponse_2("http://localhost:8000//api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=2&rating=&rating_contains=&sort_by=-imdb_score&title=&title_contains=&writer=&writer_contains=&year=",
 "item-1").then(response);
 
@@ -168,33 +181,3 @@ getResponse('http://localhost:8000//api/v1/titles/?year=&min_year=&max_year=&imd
 getResponse_2('http://localhost:8000//api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Animation&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
                 "item-4")
 
-console.log('final',best_film_list)
-
-// let data = [];
-// for (x = 0; x < 5; x++){
-//   data.append([best_film_list.results[x]['title'], best_film_list.results[x]['year'], best_film_list.results['imdb_score'], best_film_list.results[x]['votes'], best_film_list.results[x]['directors'], best_film_list.results[x]['actors'], best_film_list.results[x]['writers'], best_film_list.results[x]['genres']]);
-//   console.log(data)
-// };
-
-
-
-var first_list_film = []
-// const promise = new Promise((resolve,reject) => {
-  // const best_film_list = getResponse('http://localhost:8000//api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
-  // "item-1");
-//   test_modal("item-1");
-
-//   if(best_film_list) {
-//       resolve(best_film_list)
-    
-//   }
-// })
-
-// promise.then((best_film_list) =>{
-
-//   console.log("1",best_film_list)
-//   first_list_film = best_film_list.results
-//   return first_list_film
-// })
-// first_list_film = promise.resolve();
-// console.log(first_list_film)
