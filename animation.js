95012f491fc193582af497424cfa8e9df62c0e42
Mehdi <mehditmz1@gmail.com>
2022-11-09 02:18:25 +0100

    best categorie updated and first film removed

--- animation.js
+++ animation.js
@@ -40,7 +40,7 @@ async function test(id) {
 
   
 }
-async function getResponse(url,item, main = false) {
+async function getResponse(url, item, main = false, best_categorie = false) {
     return await fetch(
       url,{
         method: 'GET',
@@ -54,9 +54,20 @@ async function getResponse(url,item, main = false) {
           console.log(value);
           if (main){
             document.getElementsByClassName("Main-film-container")[0].style = "background-image: url('" + value.results[0]["image_url"]+"')";        
-          } 
-          charge_image(item, value.results);
-          test_modal(item,value.results)
+          }
+          image_table = document.getElementsByClassName(item)
+          if (best_categorie){
+            image_table[0].src = value.results[1]["image_url"]
+            image_table[1].src = value.results[2]["image_url"]
+            image_table[2].src = value.results[3]["image_url"]
+            image_table[3].src = value.results[4]["image_url"]
+          }
+          else {
+            charge_image(item, value.results);
+            test_modal(item,value.results)
+          }
+
+
         })
         .catch(function(err) {
           // Une erreur est survenue
@@ -64,7 +75,7 @@ async function getResponse(url,item, main = false) {
 
     
   }
-async function getResponse_2(url,item) {
+async function getResponse_2(url, item, best_categorie = false) {
     return await fetch(
       url,{
         method: 'GET',
@@ -76,8 +87,18 @@ async function getResponse_2(url,item) {
         })
         .then(function(value) {
           console.log(value);
-          charge_image_2(item, value.results);
-          modif_modal(item,value.results)
+          if (best_categorie){
+            image_table = document.getElementsByClassName(item)
+            image_table[4].src = value.results[0]["image_url"]
+            image_table[5].src = value.results[1]["image_url"]
+            image_table[6].src = value.results[2]["image_url"]
+            best_film_modif_modal(item, value.results)
+          }
+          else {
+            charge_image_2(item, value.results);
+            modif_modal(item, value.results)
+          }
+
         })
         .catch(function(err) {
           // Une erreur est survenue
@@ -87,12 +108,25 @@ async function getResponse_2(url,item) {
   }
 
 function display_modal_text(modal,data){
+
   modal_text = modal.getElementsByTagName("p")
   modal_text_table = recuperate_data(data)
   modal.getElementsByClassName('modal-image')[0].src = data["image_url"]
+  
   for(j = 0; j < modal_text.length; j++){
     modal_text[j].textContent = modal_text_table[j];
   }
+}
+function best_film_cat_modal(btn_name, data){
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    // Get the the modal name
+    var modal_img = modal.getElementsByTagName("modal-image")
+    var btn = document.getElementsByClassName(btn_name);
+  
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+  
 }
 function test_modal(btn_name, data){
   // Get the modal
@@ -161,12 +195,33 @@ function modif_modal(btn_name, data){
   }
 
 }
+function best_film_modif_modal(btn_name, data){
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementsByClassName(btn_name);
+  
+  btn[4].onclick = function click(){
+    modal.style.display = "block";
+    display_modal_text(modal, data[0]);
+    test([data[0]['id']])
+  }
+  btn[5].onclick = function click(){
+    modal.style.display = "block";
+    display_modal_text(modal, data[1]);
+    test([data[1]['id']])
+  }
+  btn[6].onclick = function click(){
+    modal.style.display = "block";
+    display_modal_text(modal, data[1]);
+    test([data[2]['id']])
+  }
+
+}
 var response;
 
 getResponse('http://localhost:8000//api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
-"item-1",true).then(response);
+"item-1", true, true).then(response);
 getResponse_2("http://localhost:8000//api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=2&rating=&rating_contains=&sort_by=-imdb_score&title=&title_contains=&writer=&writer_contains=&year=",
-"item-1").then(response);
+"item-1", true).then(response);
 
 getResponse('http://localhost:8000//api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=action&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
                 "item-2");
