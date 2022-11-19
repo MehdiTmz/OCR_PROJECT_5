7bf6bdc3f97e1dce066085f0bf57ac574514c63c
Mehdi <mehditmz1@gmail.com>
2022-11-19 16:05:40 +0100

    final version and comments added

--- animation.js
+++ animation.js
@@ -1,5 +1,16 @@
-function charge_image(item,list){
+function right_arrow(number){
+  // manage the click of the right arrow
+  const container = document.getElementsByClassName('scrollmenu')[number];
+  container.scrollLeft += 100;
+}
 
+function left_arrow(number){
+  // manage the click of the left arrow
+  const container = document.getElementsByClassName('scrollmenu')[number]
+  container.scrollLeft -= 100;
+}
+function charge_image(item,list){
+    // Change the 5 first image of the categorie
     image_table = document.getElementsByClassName(item)
     number_of_image = image_table.length
     for(i = 0; i < list.length; i++){
@@ -8,15 +19,18 @@ function charge_image(item,list){
 }
 
 function charge_image_2(item,list){
+  // Change the last 2 images of the categorie
   image_table = document.getElementsByClassName(item)
   image_table[5].src = list[0]["image_url"]
   image_table[6].src = list[1]["image_url"]
 }
 
 function recuperate_data(film){
-            return ["Titre : " + film['title'], "Année : " + film['year'], "Score Imdb : "  + film['imdb_score'], "Nombre de vote : " + film['votes'], "Directeur : " + film['directors'], "Atceur : " +film['actors'], "Auteur : " + film['writers'], "Genres : " + film['genres'],"Durée : " + film["duration"], "Pays : " +  film['countries'], "Date de sortie : " + film["date_published"], "Resumé : " + film["long_description"], "Rated : " + film["rated"]];
+  // Recuperate sepcifique information about a film
+            return ["Titre : " + film['title'], "Année : " + film['year'], "Score Imdb : "  + film['imdb_score'], "Nombre de vote : " + film['votes'], "Directeur : " + film['directors'], "Atceur : " +film['actors'], "Auteur : " + film['writers'], "Genres : " + film['genres'],"Durée : " + film["duration"], "Pays : " +  film['countries'], "Date de sortie : " + film["date_published"], "Resumé : " + film["long_description"], "Rated : " + film["rated"], "Box office : " + film["worldwide_gross_income"]];
 }
 async function test(id) {
+  // Request the data of a specifique film
   return await fetch(
     'http://localhost:8000/api/v1/titles/'+id,{
       method: 'GET',
@@ -41,7 +55,8 @@ async function test(id) {
   
 }
 async function getResponse(url, item, main = false, best_categorie = false) {
-    return await fetch(
+  // Call the API and take the information from it  
+  return await fetch(
       url,{
         method: 'GET',
         }
@@ -53,6 +68,7 @@ async function getResponse(url, item, main = false, best_categorie = false) {
         .then(function(value) {
           console.log(value);
           if (main){
+            // Part for the main image
             var main_container = document.getElementsByClassName("Main-film-container");
             main_container[0].style = "background-image: url('" + value.results[0]["image_url"]+"')";
             main_container[0].getElementsByClassName("Main-button")[0].onclick = function click(){
@@ -64,6 +80,7 @@ async function getResponse(url, item, main = false, best_categorie = false) {
           }
           image_table = document.getElementsByClassName(item)
           if (best_categorie){
+            // Case of the best categorie (first image removed)
             image_table[0].src = value.results[1]["image_url"]
             image_table[1].src = value.results[2]["image_url"]
             image_table[2].src = value.results[3]["image_url"]
@@ -71,6 +88,7 @@ async function getResponse(url, item, main = false, best_categorie = false) {
             best_film_cat_modal(item, value.results)
           }
           else {
+            // general case
             charge_image(item, value.results);
             test_modal(item,value.results)
           }
@@ -78,13 +96,13 @@ async function getResponse(url, item, main = false, best_categorie = false) {
 
         })
         .catch(function(err) {
-          // Une erreur est survenue
         });
 
     
   }
 async function getResponse_2(url, item, best_categorie = false) {
-    return await fetch(
+  // get the second page of the API for this categorie
+  return await fetch(
       url,{
         method: 'GET',
         }
@@ -96,6 +114,7 @@ async function getResponse_2(url, item, best_categorie = false) {
         .then(function(value) {
           console.log(value);
           if (best_categorie){
+            // Case of the best categorie (first image removed)
             image_table = document.getElementsByClassName(item)
             image_table[4].src = value.results[0]["image_url"]
             image_table[5].src = value.results[1]["image_url"]
@@ -104,20 +123,20 @@ async function getResponse_2(url, item, best_categorie = false) {
 
           }
           else {
+            // general case
             charge_image_2(item, value.results);
             modif_modal(item, value.results)
           }
 
         })
         .catch(function(err) {
-          // Une erreur est survenue
         });
 
     
   }
 
 function display_modal_text(modal,data){
-
+  // add the film picture on the modal
   modal_text = modal.getElementsByTagName("p")
   modal_text_table = recuperate_data(data)
   modal.getElementsByClassName('modal-image')[0].src = data["image_url"]
@@ -220,6 +239,7 @@ function test_modal(btn_name, data){
   
 }
 function modif_modal(btn_name, data){
+  // manage the button for the last 2 images
   var modal = document.getElementById("myModal");
   var btn = document.getElementsByClassName(btn_name);
   
@@ -234,6 +254,7 @@ function modif_modal(btn_name, data){
 
 }
 function best_film_modif_modal(btn_name, data){
+  // Manage the modal for the best film
   var modal = document.getElementById("myModal");
   var btn = document.getElementsByClassName(btn_name);
   
